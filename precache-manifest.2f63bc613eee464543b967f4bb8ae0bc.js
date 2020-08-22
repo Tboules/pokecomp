cff0e6b45ef09c362fd2a5f4e7e83f1839901e1d
Tony Boules <tboules@gmail.com>
2020-08-22 11:47:13 -0700

    Updates

---
+++ precache-manifest.2f63bc613eee464543b967f4bb8ae0bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7a5c5ffd1f03164fec4250ee6452e5e",
+    "url": "/pokecomp/index.html"
+  },
+  {
+    "revision": "28bda63a1db7a54ddb70",
+    "url": "/pokecomp/static/css/main.44535452.chunk.css"
+  },
+  {
+    "revision": "bf25630ad5dd7826cb09",
+    "url": "/pokecomp/static/js/2.03be5295.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokecomp/static/js/2.03be5295.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28bda63a1db7a54ddb70",
+    "url": "/pokecomp/static/js/main.86fa200c.chunk.js"
+  },
+  {
+    "revision": "7e6c673f135cf4aa857f",
+    "url": "/pokecomp/static/js/runtime-main.b025b0d5.js"
+  },
+  {
+    "revision": "f7f22219d53ccab8dc26826118994404",
+    "url": "/pokecomp/static/media/f4d1593471d222ddebd973210265762a.f7f22219.woff"
+  }
+]);
